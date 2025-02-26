cc36bf10f6d7a30c1ddfa3c709fb42600784b50b
rtpop <romanatab@hotmail.com>
2025-02-26 09:16:11 +0100

    separating functions into different scripts

---
+++ src/filter_panda.py
@@ -0,0 +1,24 @@
+# Import libraries
+import argparse
+import pandas as pd
+import eland
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Process and filter panda network.")
+
+    # Parse args for the main function
+    parser.add_argument('prior_file', type=str, help='Path to edge list file.')
+    parser.add_argument('input_file', type=str, help='Path to edge list file.')
+    parser.add_argument('output_file', type=str, help='Path to output file.')
+    parser.add_argument('delimiter', type=str, help='Delimiter used in the edge list files')
+    
+    return parser.parse_args()
+
+def main():
+    args = parse_arguments()
+
+    # process panda result into edgelist
+    eland.filter_panda.filter_panda(args.prior_file, args.input_file, args.output_file, sep = args.delimiter)
+
+if __name__ == "__main__":
+    main()
---
+++ src/process_panda.py
@@ -0,0 +1,23 @@
+# Import libraries
+import argparse
+import pandas as pd
+import eland
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Process and filter panda network.")
+
+    # Parse args for the main function
+    parser.add_argument('input_file', type=str, help='Path to edge list file.')
+    parser.add_argument('output_file', type=str, help='Path to output file.')
+    parser.add_argument('delimiter', type=str, help='Delimiter used in the edge list files')
+    
+    return parser.parse_args()
+
+def main():
+    args = parse_arguments()
+
+    # process panda result into edgelist
+    eland.process_edge_list(args.input_file, args.output_file, sep = args.delimiter)
+
+if __name__ == "__main__":
+    main()

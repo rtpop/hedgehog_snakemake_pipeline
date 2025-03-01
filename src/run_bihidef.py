1b8165c9d1b02ec961b13be0003a20c136fb46d5
rtpop <romanatab@hotmail.com>
2025-03-01 10:27:27 +0100

    works

--- src/run_bihidef.py
+++ src/run_bihidef.py
@@ -1,19 +1,38 @@
 import argparse
 import os
-from ELAND import bihidef
+import bihidef
+import sys
+print("Python path:", sys.path)
 
 def parse_args():
     parser = argparse.ArgumentParser("Run bihidef")
-    parser.add_argument("edgelist_file", required=True, type = str, help="Input file")
-    parser.add_argument("max_res", type = float, help="The maximum resolution to use.")
-    parser.add_argument("max_communities", type = int, help="The maximum number of communities to find.")
-    parser.add_argument("output_dir", type = str, help="Output directory")
-    parser.add_argument("output_prefix_reg", type = str, help="Output prefix for regulator communities.")
-    parser.add_argument("output_prefix_tar", type = str, help="Output prefix for target communities.")
+    parser.add_argument("edgelist_file", type = str, help="Input file")
+    parser.add_argument("--max_res", type = float, help="The maximum resolution to use.")
+    parser.add_argument("--comm_mult", type = float, help="The maximum number of communities to find.")
+    parser.add_argument("--output_dir", type = str, help="Output directory.")
+    parser.add_argument("--output_prefix_reg", type = str, help="Output prefix for regulator communities.")
+    parser.add_argument("--output_prefix_tar", type = str, help="Output prefix for target communities.")
     return parser.parse_args()
 
 def main():
     args = parse_args()
     
+    # change to output directory
+    os.chdir(args.output_dir)
+    
+    # edit input file path to be relative to output directory
+    args.edgelist_file = os.path.join("../../..", args.edgelist_file)
+    
+    print("Running bihidef with the following parameters:")
+    print("Input file: ", args.edgelist_file)
+    print("Max resolution: ", args.max_res)
+    print("Communities multiplier: ", args.comm_mult)
+    print("Output directory: ", args.output_dir)
+    print("Output prefix for regulator communities: ", args.output_prefix_reg)
+    print("Output prefix for target communities: ", args.output_prefix_tar)
+    
     # run bihidef
-    bihidef.bihidef(args.edgelist_file, args.max_res, args.max_communities, args.output_dir, args.output_prefix_reg, args.output_prefix_tar)
+    bihidef.bihidef(filename = args.edgelist_file, maxres = args.max_res, comm_mult = args.comm_mult, oR= args.output_prefix_reg, oT = args.output_prefix_tar)
+
+if __name__ == "__main__":
+    main()

29979e7be02d0ca10101c45bd015f66ec492031b
rtpop <romanatab@hotmail.com>
2025-03-05 11:54:41 +0100

    fixed param parsing

--- src/analysis/GO_enrichment.R
+++ src/analysis/GO_enrichment.R
@@ -17,11 +17,11 @@ options(stringsAsFactors = FALSE)
 option_list <- list(
     optparse::make_option(c("-g", "--gmt-file"), type = "character", help = "GMT file with the communities."),
     optparse::make_option(c("-b", "--bg-file"), type = "character", help = "Path to a file with the background genes. Only needed if auto_bg is FALSE."),
-    optparse::make_option(c("-a", "--auto-bg"), type = "logical", default = TRUE, help = "If TRUE, the background is automatically set to the genes in the gene sets.")
-    optparse::make_option(c("-s", "--save-all"), type = "logical", default = TRUE, help = "If TRUE, the GO enrichment for each community will be saved in addition to a summary for all communities.")
-    optparse::make_option(c("-t", "--sig-thresh"), type = "numeric", default = 0.05, help = "P-value significant threshold")
-    optparse::make_option(c("-st", "--statistic"), type = "character", default = "fisher", help = "Test statistic to be used.")
-    optparse::make_option(c("-ag", "--algorithm"), type = "character", default = "classic", help = "topGO algorithm to use for enrichment.")
+    optparse::make_option(c("-a", "--auto-bg"), type = "logical", default = TRUE, help = "If TRUE, the background is automatically set to the genes in the gene sets."),
+    optparse::make_option(c("-s", "--save-all"), type = "logical", default = TRUE, help = "If TRUE, the GO enrichment for each community will be saved in addition to a summary for all communities."),
+    optparse::make_option(c("-t", "--sig-thresh"), type = "numeric", default = 0.05, help = "P-value significant threshold"),
+    optparse::make_option(c("--statistic"), type = "character", default = "fisher", help = "Test statistic to be used."),
+    optparse::make_option(c("--algorithm"), type = "character", default = "classic", help = "topGO algorithm to use for enrichment.")
 )
 
 opt_parser <- optparse::OptionParser(option_list = option_list)
@@ -31,11 +31,12 @@ opt <- optparse::parse_args(opt_parser)
 ## Variables ##
 ## --------- ##
 
-GMT_FILE <- opt$gmt_file
-BG_FILE <- opt$bg_file
-AUTO_BG <- opt$auto_bg
-SAVE <- opt$save_all
-THRESHOLD <- opt$sig_thresh
+
+GMT_FILE <- opt$`gmt-file`
+BG_FILE <- opt$`bg-file`
+AUTO_BG <- opt$`auto-bg`
+SAVE <- opt$`save-all`
+THRESHOLD <- opt$`sig-thresh`
 STAT <- opt$statistic
 ALG <- opt$algorithm
 

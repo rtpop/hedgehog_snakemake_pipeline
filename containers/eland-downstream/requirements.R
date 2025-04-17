a4f96c7da1efbce2572d768b760f5b4d9f531f6a
rtpop <romanatab@hotmail.com>
2025-04-17 09:11:13 +0200

    added libs

--- containers/eland-downstream/requirements.R
+++ containers/eland-downstream/requirements.R
@@ -6,20 +6,23 @@
 required_packages_cran = c(
     "BiocManager",    # To install Bioconductor packages
     # "curl",           # For downloading files
-    # "dplyr",          # For data manipulation
+    "dplyr",          # For data manipulation
     "data.table",     # For data manipulation with data.table
     "optparse",       # For reading command-line arguments
     "devtools",       # For installing R packages from GitHub
-    # "purrr",          # For functional programming tools
+    "purrr",          # For functional programming tools
     # "rcartocolor",    # For color palettes for maps and plots
     # "reshape2",       # For reshaping data frames
     # "this.path",      # For creating relative paths
-    # "tidyr",          # For tidying data
+    "tidyr",          # For tidying data
     "ggplot2",        # For creating plots
     "ggrepel",        # For adding labels to ggplot2 plots
+    "ggpubr",         # For ggplot2 publication-ready plots
     "sessioninfo",    # For session information
     "pheatmap",       # For drawing heatmaps
-    "RColorBrewer"   # For color palettes for heatmaps
+    "RColorBrewer",   # For color palettes for heatmaps
+    "stringr",        # For string manipulation
+    "magrittr",       # For pipe operator
     # stylo,            # For cosine distance calculation
     # vegan           # For binomial distance calculation
 )

e3a3ec908c491d01fba672ed70f69f478a9bcfc8
rtpop <romanatab@hotmail.com>
2025-05-02 13:05:09 +0200

    updated to get edges as well

--- src/process_data/process_gtex_exp.R
+++ src/process_data/process_gtex_exp.R
@@ -5,21 +5,37 @@
 
 rm(list=ls())
 
-#' @name get_gtex_exp
+#' @name get_gtex_data
 #' 
-#' @param exp Data frame with gene expression data.
-#' @param samples Data frame with sample information.
-#' @param genes Data frame with gene information.
-#' 
-get_gtex_exp <- function(exp, samples, genes) {
+#' @param data RData file containing GTEx data.
+#' @param extract_edges Logical. If edges should be extracted for each tissue. If FALSE,
+#' expression will be extracted instead
+
+get_gtex_data <- function(data, extract_edges = TRUE) {
+    # Load the data
+    load(data)
 
     tissues <- as.character(unique(samples$Tissue))
+    data.table::fwrite(edges, file = "data/motif_prior.txt", sep = ",", row.names = FALSE, col.names = FALSE)
 
     for (i in tissues) {
         tissue_samples <- samples[samples$Tissue == i, 1]
-        tissue_exp <- data.frame(exp[, tissue_samples])
-        gene_names <- as.character(genes[which(genes$Name == rownames(tissue_exp)), 1])
-        tissue_exp <- cbind(gene_names, tissue_exp)
+        
+
+        if (extract_edges) {
+            # Extract edges for the specific tissue
+            res <- net[, i]
+            res <- cbind(edges[,1:2], res)
+            gene_names <- as.character(genes[which(genes$Name == rownames(res)), 1])
+            res[,1] <- gene_names
+            file_name <- paste0("panda_network_edgelist.txt")
+        } else {
+            # Extract expression data for the specific tissue
+            res <- data.frame(exp[, tissue_samples])
+            gene_names <- as.character(genes[which(genes$Name == rownames(res)), 1])
+            res <- cbind(gene_names, res)
+            file_name <- paste0("exp_", i, ".txt")
+        }
 
         # Create directory if it doesn't exist
         dir_path <- file.path("data", i)
@@ -28,10 +44,9 @@ get_gtex_exp <- function(exp, samples, genes) {
         }
 
         # Save the data with row names
-        data.table::fwrite(tissue_exp, file = file.path(dir_path, paste0("gtex_", i, "_exp.tsv")), sep = "\t")
+        data.table::fwrite(res, file = file.path(dir_path, file_name), sep = ",", row.names = FALSE, col.names = FALSE)
     }
 }
 
 data <- "/storage/kuijjerarea/romana/eland/ELAND/data/GTEx_PANDA_net.RData"
-load(data)
-get_gtex_exp(exp, samples, genes)
+get_gtex_data(data)
